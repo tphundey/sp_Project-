499a3af0ebdd2cb56a502cfbc6b6e56eab480c81
Your Name <105340771+tphundey@users.noreply.github.com>
2024-03-10 17:11:28 +0700

    sss

--- Inclusive_ELearning/client/src/pages/user/MyLearningPage/ChildrenMyLearning/History/History.tsx
+++ Inclusive_ELearning/client/src/pages/user/MyLearningPage/ChildrenMyLearning/History/History.tsx
@@ -1,148 +1,185 @@
 import { useState, useEffect } from 'react';
-import { Empty, Pagination } from 'antd';
-import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
-import { initializeApp, FirebaseApp } from 'firebase/app';
 import { firebaseConfig } from '@/components/GetAuth/firebaseConfig';
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { Empty } from 'antd';
 
-const app: FirebaseApp = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 const History = () => {
+    const [orders, setOrders] = useState([]);
+    const [courses, setCourses] = useState({});
+    const [email, setEmail] = useState<string | null>(null);
     const [user, setUser] = useState<User | null>(null);
-    const [email, setEmail] = useState<any | null>(null);
-    const [userId, setUserId] = useState<any | null>(null);
+    const [userId, setUserID] = useState<string | null>(null);
     const [loading, setLoading] = useState(true);
-    const [savedCourses, setSavedCourses] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser: any) => {
             setUser(currentUser);
             setEmail(currentUser?.email);
-            setUserId(currentUser?.uid)
-
             setLoading(false);
         });
         return () => {
             unsubscribe();
         };
-    }, [auth]);
+    }, []);
+
+    useEffect(() => {
+        if (email) {
+            fetch(`http://localhost:3000/googleAccount?email=${email}`)
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error('Failed to fetch user data from API.');
+                    }
+                })
+                .then((userDataArray) => {
+                    if (userDataArray.length > 0) {
+                        const userData = userDataArray[0];
+                        setUserID(userData.id);
+                    }
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    }, [email]);
     console.log(userId);
 
     useEffect(() => {
-        fetch(`http://localhost:3000/googleAccount/${userId}`)
-            .then((response) => response.json())
-            .then((userData) => {
-                if (userData) {
-                    const user = userData;
-                    const savedCourseIds = user.historyCourseID;
-                    fetch(`http://localhost:3000/Courses`)
-                        .then((response) => response.json())
-                        .then((coursesData) => {
-                            const savedCourses = coursesData.filter((course: any) =>
-                                savedCourseIds.includes(course.id)
-                            );
-                            setSavedCourses(savedCourses);
-                        })
-                        .catch((error) => {
-                            console.error('Error fetching courses: ', error);
-                        });
+        fetch(`http://localhost:3000/Payment`)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
                 }
+                throw new Error('Failed to fetch orders');
             })
-            .catch((error) => {
-                console.error('Error fetching user data: ', error);
+            .then(data => {
+                // Filter orders based on userId
+                const filteredOrders = data.filter(order => order.userId === userId);
+                // Reverse the order of the filteredOrders array
+                filteredOrders.reverse();
+                // Kiểm tra và xử lý cấu trúc không đồng nhất của items
+                filteredOrders.forEach(order => {
+                    if (typeof order.items === 'string') {
+                        order.items = JSON.parse(order.items);
+                    }
+                });
+                setOrders(filteredOrders);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching orders:', error);
+                setLoading(false);
             });
-    }, [email]
-    );
+    }, [userId]);
 
-    const handleRemoveCourse = (courseId: any) => {
-        fetch(`http://localhost:3000/googleAccount/${userId}`)
-            .then((response) => response.json())
-            .then((userData: any) => {
-                if (userData) {
-                    const user = userData;
-                    const registeredCourseIds = user.historyCourseID;
-                    const updatedRegisteredCourseIds = registeredCourseIds.filter((id: any) => id !== courseId);
-                    const updatedUserData = { ...user, historyCourseID: updatedRegisteredCourseIds };
-                    fetch(`http://localhost:3000/googleAccount/${user.id}`, {
-                        method: 'PUT', // Hoặc PATCH tùy thuộc vào cấu trúc của API
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify(updatedUserData),
-                    })
-                        .then((response: any) => {
-                            if (response.ok) {
-                                // Cập nhật lại danh sách khóa học đã đăng ký sau khi xóa
-                                const updatedSavedCourses = savedCourses.filter((course: any) => course.id !== courseId);
-                                setSavedCourses(updatedSavedCourses);
-                            } else {
-                                console.error('Failed to update user data:', response);
-                            }
-                        })
-                        .catch((error) => {
-                            console.error('Error updating user data:', error);
-                        });
+    useEffect(() => {
+        fetch("http://localhost:3000/Courses")
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
                 }
-            }), [email]
+                throw new Error('Failed to fetch courses');
+            })
+            .then(data => {
+                // Reverse the order of the courses array
+                data.reverse();
+                const coursesMap = {};
+                data.forEach(course => {
+                    coursesMap[course.id] = course;
+                });
+                setCourses(coursesMap);
+            })
+            .catch(error => {
+                console.error('Error fetching courses:', error);
+            });
+    }, [userId]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+    const cancelOrderAndUpdateStatus = (orderId) => {
+        // Call the updateOrderStatus function to update the order status
+        updateOrderStatus(orderId, 'Cancelled');
     };
 
-    const itemsPerPage = 2;
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const coursesToDisplay = savedCourses.slice(startIndex, endIndex);
-
-    const handlePageChange = (page: any) => {
-        setCurrentPage(page);
+    const updateOrderStatus = (orderId, newStatus) => {
+        // Implement your updateOrderStatus function here
+        fetch(`http://localhost:3000/Payment/${orderId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                status: newStatus,
+            }),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to update order status');
+                }
+                // Assuming you want to refresh the page after updating the status
+                window.location.reload();
+            })
+            .catch(error => {
+                console.error('Error updating order status:', error);
+                // Handle errors or display a message to the user if necessary
+            });
     };
-
     return (
-        <div className="listProgress">
-            {savedCourses.length > 0 ? (
-                coursesToDisplay.reverse().map((course: any) => (
-                    <div className='ty-contai'>
-                        {/* Hiển thị thông tin khóa học */}
-                        <div className="courseProgress">
-                            <div className="imgCoureProgress">
-                                <img src={course.courseIMG} alt="" />
-                            </div>
-                            <div className="infoCourseProgress">
-                                <h3>COURSE</h3>
-                                <a href={`/introduction/${course.id}`}> <h2>{course.courseName}</h2></a>
-                                <div className="fl-info-progress">
-                                    <div className="fl1-info-progress">
-                                        <img className='mt-1' src="https://f63-zpg-r.zdn.vn/4940067705430501247/8f148f0e98874fd91696.jpg" alt="" />
-                                    </div>
-                                    <div className="fl2-info-progress">
-                                        LinkedIn - By: Trần Phùng
+        <div className='orders'>
+            {orders.length > 0 ? (
+                <ul>
+                    {orders.map(order => {
+                        // Check if the order status is one of the statuses you want to exclude
+                        if (order.status === "Đơn hàng chờ xác nhận" || order.status === "Đơn hàng đang được đóng gói" || order.status === "Đơn hàng đang được giao cho đơn vị vận chuyển" || order.status === "Đơn hàng đang được giao" || order.status === "Đơn hàng đang hoàn trả") {
+                            // If the status matches, don't render this order
+                            return null;
+                        }
+                        // If the status doesn't match, render the order
+                        return (
+                            <li className='ord-map' style={{ width: 700 }} key={order.id}>
+                                <h3 className='text-xs ml-12 mb-3'>Mã đơn hàng: {order.id}</h3>
+                                <ul>
+                                    {order.items.map((item: any, index: any) => (
+                                        <li key={index}>
+                                            <div className='flex pro-cr'>
+                                                {courses[item.productId] && courses[item.productId].courseIMG ? (
+                                                    <img width={120} src={courses[item.productId].courseIMG[0]} alt="" />
+                                                ) : (
+                                                    <div>No image available</div>
+                                                )}
+                                                <div>
+                                                    <p className='font-medium'>{courses[item.productId]?.courseName || 'Unknown Course'}</p>
+                                                    Kích cỡ: {item.size}, Màu sắc: {item.color}, Số lượng: {item.quantity} đôi
+                                                </div>
+                                            </div>
+                                        </li>
+                                    ))}
+
+                                </ul>
+                                <div className='ml-12'>
+                                    <p>Thành tiền: <span className='text-red-500 font-medium'>đ{order.amount}</span></p>
+                                    <div className="flex justify-between">
+                                        <div>
+                                            <p className='text-green-600 font-medium'> <i className="fa-solid fa-truck-fast"></i> {order.status}</p>
+                                            <p className='text-sm'>Trạng thái thanh toán: {order.status2}</p>
+                                            <p className='text-sm'>Trạng thái thanh toán: {order.option}</p>
+                                           
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="option-course-progress">
-                            <div className="dropdown dropdown-right dropdown-end mt-5">
-                                <label tabIndex={0} className="btn m-1"><i className="fa-solid fa-ellipsis"></i></label>
-                                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 right-0 mt-8">
+                            </li>
+                        );
+                    })}
+                </ul>
 
-                                    <li><a onClick={() => handleRemoveCourse(course.id)}>Remove</a></li> {/* Thêm hàm xử lý xóa */}
-                                </ul>
-                            </div>
-                        </div>
-                        <hr />
-                    </div>
-                ))
             ) : (
                 <Empty className='mt-20' />
-            )}
-            {savedCourses.length > itemsPerPage && (
-                <Pagination
-                    className='mb-3'
-                    defaultCurrent={1}
-                    total={savedCourses.length}
-                    pageSize={itemsPerPage}
-                    current={currentPage}
-                    onChange={handlePageChange}
-                />
             )}
         </div>
     );
--- Inclusive_ELearning/client/src/pages/user/MyLearningPage/ChildrenMyLearning/InProgress/InProgress.tsx
+++ Inclusive_ELearning/client/src/pages/user/MyLearningPage/ChildrenMyLearning/InProgress/InProgress.tsx
@@ -102,45 +102,82 @@ const InProgress = () => {
     if (loading) {
         return <div>Loading...</div>;
     }
+    const cancelOrderAndUpdateStatus = (orderId) => {
+        // Call the updateOrderStatus function to update the order status
+        updateOrderStatus(orderId, 'Đơn hàng đã hủy');
+    };
 
+    const updateOrderStatus = (orderId, newStatus) => {
+        // Implement your updateOrderStatus function here
+        fetch(`http://localhost:3000/Payment/${orderId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                status: newStatus,
+            }),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to update order status');
+                }
+                // Assuming you want to refresh the page after updating the status
+                window.location.reload();
+            })
+            .catch(error => {
+                console.error('Error updating order status:', error);
+                // Handle errors or display a message to the user if necessary
+            });
+    };
     return (
         <div className='orders'>
             {orders.length > 0 ? (
                 <ul>
-                    {orders.map(order => (
-                        <li className='ord-map' style={{ width: 700 }} key={order.id}>
-                            <h3 className='text-xs ml-12 mb-3'>Mã đơn hàng: {order.id}</h3>
-                            <ul>
-                                {order.items.map((item: any, index: any) => (
-                                    <li key={index}>
-                                        <div className='flex pro-cr'>
-                                            {courses[item.productId] && courses[item.productId].courseIMG ? (
-                                                <img width={120} src={courses[item.productId].courseIMG[0]} alt="" />
-                                            ) : (
-                                                <div>No image available</div>
-                                            )}
-                                            <div>
-                                                <p className='font-medium'>{courses[item.productId]?.courseName || 'Unknown Course'}</p>
-                                                Kích cỡ: {item.size}, Màu sắc: {item.color}, Số lượng: {item.quantity} đôi
+                    {orders.map(order => {
+                        // Check if the order status is one of the statuses you want to exclude
+                        if (order.status === "Đơn hàng đã được giao thành công" || order.status === "Đơn hàng đã hủy") {
+                            // If the status matches, don't render this order
+                            return null;
+                        }
+                        // If the status doesn't match, render the order
+                        return (
+                            <li className='ord-map' style={{ width: 700 }} key={order.id}>
+                                <h3 className='text-xs ml-12 mb-3'>Mã đơn hàng: {order.id}</h3>
+                                <ul>
+                                    {order.items.map((item: any, index: any) => (
+                                        <li key={index}>
+                                            <div className='flex pro-cr'>
+                                                {courses[item.productId] && courses[item.productId].courseIMG ? (
+                                                    <img width={120} src={courses[item.productId].courseIMG[0]} alt="" />
+                                                ) : (
+                                                    <div>No image available</div>
+                                                )}
+                                                <div>
+                                                    <p className='font-medium'>{courses[item.productId]?.courseName || 'Unknown Course'}</p>
+                                                    Kích cỡ: {item.size}, Màu sắc: {item.color}, Số lượng: {item.quantity} đôi
+                                                </div>
                                             </div>
-                                        </div>
-                                    </li>
-                                ))}
+                                        </li>
+                                    ))}
 
-                            </ul>
-                            <div className='ml-12'>
-                                <p>Thành tiền: <span className='text-red-500 font-medium'>đ{order.amount}</span></p>
-                                <div className="flex justify-between">
-                                    <div>
-                                        <p className='text-green-600 font-medium'> <i className="fa-solid fa-truck-fast"></i> {order.status}</p>
-                                        <p className='text-sm'>Trạng thái thanh toán: {order.status2}</p>
-                                        <p className='text-sm'>Trạng thái thanh toán: {order.option}</p>
+                                </ul>
+                                <div className='ml-12'>
+                                    <p>Thành tiền: <span className='text-red-500 font-medium'>đ{order.amount}</span></p>
+                                    <div className="flex justify-between">
+                                        <div>
+                                            <p className='text-green-600 font-medium'> <i className="fa-solid fa-truck-fast"></i> {order.status}</p>
+                                            <p className='text-sm'>Trạng thái thanh toán: {order.status2}</p>
+                                            <p className='text-sm'>Trạng thái thanh toán: {order.option}</p>
+                                            <button className='bg-red-500 text-white p-2' onClick={() => cancelOrderAndUpdateStatus(order.id)}>Cancel Order</button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </li>
-                    ))}
+                            </li>
+                        );
+                    })}
                 </ul>
+
             ) : (
                 <Empty className='mt-20' />
             )}
